c55ea6a72c0f7bcac7b154f5ede545b09036461c
arjkailash <arjkailash@gmail.com>
2015-09-19 16:21:24 -0500

    1.06 made list_item_forecast

--- app/src/main/java/com/akr/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/akr/sunshine/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -15,8 +20,23 @@ public class MainActivityFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        TextView text = (TextView) view.findViewById(R.id.textView2);
+        Calendar cal = new GregorianCalendar();
+        Date today = new Date();
+        String todaysdate = today.toString();
+        String str = cal.getTime().toString();
+        text.setText(todaysdate);
+        return  view;
     }
 }
